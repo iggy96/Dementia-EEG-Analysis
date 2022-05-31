8a17af6a2969b320fed6e1fc2cc7662a987975d3
Joshua Ighalo <70718280+iggy96@users.noreply.github.com>
2022-05-31 03:08:53 -0700

    added new parameters for preprocessing pipeline

--- params.py
+++ params.py
@@ -1,5 +1,26 @@
 from df_lib import np
 
+# Analysis parameters
+figure_size = (6,16)
+plot_color = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd']
+channelNames = ['Fz','Cz','Pz','P07','OZ']
+
+# EEG parameters
+epoch = 1000  # ms - total window size
+prestim = 100  # ms - duration  of the baseline (pre-stimulus interval) within the epoch
+lowPass = 30  # Hz - cut-off for the low-pass filter
+highPass = 1  # Hz - cut-off for the high-pass filter
+line = 60  # Hz - line voltage
+clip = 75  # uV - clipping threshold
+fs = 500  # todo - move this to the hardware config
+stimTrig = dict(std=[1], dev=[2], con=[4, 7], inc=[5, 8])
+stimTypes = ['std', 'dev', 'con', 'inc'] 
+stimNum = dict(std = [264], dev = [24], con = [36], inc = [36])
+
+erpTypes = ['N100', 'P300', 'N400']
+erpStim = dict(N100 = ['std', 'dev'], P300 = ['std', 'dev'], N400 = ['con', 'inc'])
+erpRange = dict(N100 = [70, 200], P300 = [200, 500], N400 = [300, 800])
+
 ant = dict(
     fs=500,                            # Hz - sampling rate of the amplifier
     dataCols=12,                       # Number of columns in the EEG
