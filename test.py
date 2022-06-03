24c307b02d39e54654835ce44507067867e57974
Joshua Ighalo <70718280+iggy96@users.noreply.github.com>
2022-06-03 00:07:30 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 # test pipeline for the bruyere dataset
 
 from fn_cfg import *
+import params as cfg
 
 localPath = '/Volumes/Backup Plus/EEG_Datasets/laurel_place/dataset'
 filename = '0002_2_12122019_1225'
@@ -19,16 +20,19 @@ fileObjects = device.init(version,filename,localPath)
 rawEEG = fileObjects[0]
 rawEOG = fileObjects[1]
 rawEEGEOG = fileObjects[2]
-time_period = fileObjects[3]
+time = fileObjects[3]
 trigChannel = fileObjects[4]
 
-#plots(time_period[0:50],rawEEGEOG[0:50],titles,figsize,plot_color)
+filtering = filters()
+notchFilterOutput = filtering.notch(rawEEG,line,fs)
+plots(time,notchFilterOutput,titles=cfg.channelNames,figsize=cfg.figure_size,pltclr=cfg.plot_color)
 
+###############################################################################
 import pywt
 from pywt import wavedec, waverec
 import numpy as np
 
-eeg = rawEEG[:,1]
+original_EEG = notchFilterOutput[:,1]
 
 """
 Detection of Ocular Artifacts using DWT (haar wavelet transform)
@@ -36,7 +40,7 @@ Based on "Comparative Study of Wavelet-Based Unsupervised Ocular Artifact Remova
 and "ARDER: An Automatic EEG Artifacts Detection and Removal System"
 """
 wavelet = 'haar'
-coeffs = (wavedec(eeg,wavelet,level=5))
+coeffs = (wavedec(original_EEG,wavelet,level=5))
 
 coeffs_approx = coeffs[0]
 coeffs_5l = coeffs[1]
@@ -68,7 +72,7 @@ recov_signal[abs(recov_signal) > 100] = 0
 zones_OZ = np.where(recov_signal == 0)[0]
 
 # original signal with ocular artifacts
-ocularArtifacts = eeg[zones_OZ]
+ocularArtifacts = original_EEG[zones_OZ]
 
 plt.plot(recov_signal)
 plt.title('clean signal')
@@ -120,7 +124,6 @@ coeffs_3l[abs(coeffs_3l) > threshold_coeff8L] = 0
 coeffs_2l[abs(coeffs_2l) > threshold_coeff8L] = 0
 coeffs_1l[abs(coeffs_1l) > threshold_coeff8L] = 0
 
-
 acc_coeffs = []
 acc_coeffs.append(coeffs_approx)
 acc_coeffs.append(coeffs_8l)
@@ -152,13 +155,22 @@ mse = np.mean((denoised_ocularArtifacts-ocularArtifacts)**2)/len(ocularArtifacts
 print('Mean Squared Error (MSE):',mse)
 
 # Dump denoised OA signal inside original EEG
-s = eeg
+s = original_EEG
 l = zones_OZ
 m = denoised_ocularArtifacts
 d = dict(zip(l, m))
 oa_cleanEEG = [d.get(i, j) for i, j in enumerate(s)]
 
-plt.plot(eeg,color='red',label='original')
+# Signal to Artifact Ratio (SAR)
+sar = 10*np.log10(np.std(original_EEG)/np.std(original_EEG-oa_cleanEEG))
+print('Signal to Artifact Ratio (SAR):',sar)
+
+# Mean Square Error (MSE)
+# Smaller values of MSE means that the denoised EEG is more closed to clean EEG
+mse = np.mean((oa_cleanEEG-original_EEG)**2)/len(original_EEG)
+print('Mean Squared Error (MSE):',mse)
+
+plt.plot(original_EEG,color='red',label='original')
 plt.title('Original EEG')
 plt.show()
 plt.plot(oa_cleanEEG,color='blue',label='clean')
@@ -171,4 +183,21 @@ Detection of Muscular Artifacts
 Based on "Comparative Study of Wavelet-Based Unsupervised Ocular Artifact Removal Techniques for Single-Channel EEG Data"
 and "ARDER: An Automatic EEG Artifacts Detection and Removal System"
 """
-freqs,psd = signal.welch()
+freqs,psd = signal.welch(original_EEG,fs=500,nperseg=1024)
+
+# Extract Power Spectral Density (PSD) for 30-100 Hz and 0-100 Hz
+def find_nearest(X, value):
+    return X[np.unravel_index(np.argmin(np.abs(X - value)), X.shape)]
+
+nearest_0Hz = find_nearest(freqs,0)
+nearest_30Hz = find_nearest(freqs,30)
+nearest_100Hz = find_nearest(freqs,100)
+
+idx_0Hz = np.where(freqs==nearest_0Hz)
+idx_30Hz = np.where(freqs==nearest_30Hz)
+idx_100Hz = np.where(freqs==nearest_100Hz)
+
+psd_30_100Hz = psd[idx_30Hz[0][0]:idx_100Hz[0][0]]
+psd_0_100Hz = psd[idx_0Hz[0][0]:idx_100Hz[0][0]]
+
+mad = np.sum(psd_30_100Hz)/np.sum(psd_0_100Hz)
