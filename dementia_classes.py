7e52779d99ccf7db59a1c412bf7eb25a0ac093cf
Joshua Ighalo <70718280+iggy96@users.noreply.github.com>
2022-06-15 20:58:45 -0700

    Update dementia_classes.py

--- dementia_classes.py
+++ dementia_classes.py
@@ -2,9 +2,54 @@ from fn_cfg import *
 import params as cfg
 
 df = pd.read_excel (r'/Volumes/Backup Plus/EEG_Datasets/laurel_place/dataset/lp_clinical.xlsx')
-df_new = (df[['ID','MMSE']]).to_numpy()
+data = (df[['ID','MMSE']]).to_numpy()
 
-#   no dementia
-df_no_dementia = df_new[df_new[:,1] >= 25]
-#   mild dementia
-df_mild_dementia = df_new[df_new[:,1] 
+""""
+def oneFourDigit(x):
+    return '{:04d}'.format(x)
+
+arr = []
+for i in range(len(data)):
+    arr.append(oneFourDigit(data[i,0]))
+arr = np.array(arr)
+data = np.vstack((arr, data[:,1])).T
+"""
+
+#   no dementia (ND)
+idx_ND = np.where(data[:,1] >= 25)[0]
+data_ND = data[:,0][idx_ND]
+
+#   mild dementia (MILD)
+idx_MILD = np.where(np.logical_and(data[:,1]>=20,data[:,1]<=24))
+data_MILD = data[:,0][idx_MILD]
+
+#   moderate dementia (MOD)
+idx_MOD = np.where(np.logical_and(data[:,1]>=13,data[:,1]<=19))
+data_MOD = data[:,0][idx_MOD]
+
+#   severe dementia (SEVD)
+idx_SEVD = np.where(data[:,1]<=12)
+data_SEVD = data[:,0][idx_SEVD]
+
+#   use the seperated IDs
+def oneFourDigit(x):
+    return '{:04d}'.format(x)
+
+arr = []
+for i in range(len(data_ND)):
+    arr.append(oneFourDigit(data_ND[i]))
+data_ND = np.array(arr)
+arr = []
+for i in range(len(data_MILD)):
+    arr.append(oneFourDigit(data_MILD[i]))
+data_MILD = np.array(arr)
+arr = []
+for i in range(len(data_MOD)):
+    arr.append(oneFourDigit(data_MOD[i]))
+data_MOD = np.array(arr)
+arr = []
+for i in range(len(data_SEVD)):
+    arr.append(oneFourDigit(data_SEVD[i]))
+data_SEVD = np.array(arr)
+
+
