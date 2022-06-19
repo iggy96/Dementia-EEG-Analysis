44c294b613585e6d9c6beaa69e42ad626b431ce0
Joshua Ighalo <70718280+iggy96@users.noreply.github.com>
2022-06-18 21:42:49 -0700

    changed directory to import renamed dataset folder

--- dementia_classes.py
+++ dementia_classes.py
@@ -11,152 +11,13 @@ this script sorts the scans into the following categories:
 
 Input: .xlsx file with the following columns: scan ID and MMSE
        
-
-
-
 """
 from fn_cfg import *
 import params as cfg
-"""
-df_1 = pd.read_excel (r'/Users/joshuaighalo/Downloads/EEG_Datasets/laurel_place/dataset/lp_clinical.xlsx')
-df_2 = pd.read_excel(r'/Users/joshuaighalo/Downloads/EEG_Datasets/laurel_place/dataset/Scan_details.xlsx')
-data_1 = (df_1[['ID','MMSE']]).to_numpy()
-data_2 = (df_2[['Baseline','4 months','8 months']]).to_numpy()
-
-#   remove scans of participants who died
-died = [152,280,409]
-idx_died = np.where(np.isin(data_1[:,0], died))[0]
-data_1 = np.delete(data_1,idx_died,axis=0)
-data_2 = np.delete(data_2,idx_died,axis=0)
-
-#   no dementia (ND)
-idx_ND = np.where(data_1[:,1] >= 25)[0]
-data_ND = data_1[:,0][idx_ND]
-
-#   mild dementia (MILD)
-idx_MILD = np.where(np.logical_and(data_1[:,1]>=20,data_1[:,1]<=24))
-data_MILD = data_1[:,0][idx_MILD]
-
-#   moderate dementia (MOD)
-idx_MOD = np.where(np.logical_and(data_1[:,1]>=13,data_1[:,1]<=19))
-data_MOD = data_1[:,0][idx_MOD]
-
-#   severe dementia (SEVD)
-idx_SEVD = np.where(data_1[:,1]<=12)
-data_SEVD = data_1[:,0][idx_SEVD]
-
-#   seperate scan IDs for each dementia type
-def oneFourDigit(x):
-    return '{:04d}'.format(x)
-
-arr = []
-for i in range(len(data_ND)):
-    arr.append(oneFourDigit(data_ND[i]))
-data_ND = np.array(arr)
-arr = []
-for i in range(len(data_MILD)):
-    arr.append(oneFourDigit(data_MILD[i]))
-data_MILD = np.array(arr)
-arr = []
-for i in range(len(data_MOD)):
-    arr.append(oneFourDigit(data_MOD[i]))
-data_MOD = np.array(arr)
-arr = []
-for i in range(len(data_SEVD)):
-    arr.append(oneFourDigit(data_SEVD[i]))
-data_SEVD = np.array(arr)
-
-#   import the names of all the scans folders
-import os
-search_path = r'/Users/joshuaighalo/Downloads/EEG_Datasets/laurel_place/dataset'
-root, dirs, files = next(os.walk(search_path), ([],[],[]))
-scanFolders = np.array(dirs)
-
-#   match scan IDs from csv with scan folders
-scanFolders_Init = np.array([w[0:4] for w in scanFolders])
-idx_ND = np.where(np.isin(scanFolders_Init, data_ND))[0]
-scans_ND = scanFolders[idx_ND]
-idx_MILD = np.where(np.isin(scanFolders_Init, data_MILD))[0]
-scans_MILD = scanFolders[idx_MILD]
-idx_MOD = np.where(np.isin(scanFolders_Init, data_MOD))[0]
-scans_MOD = scanFolders[idx_MOD]
-idx_SEVD = np.where(np.isin(scanFolders_Init, data_SEVD))[0]
-scans_SEVD = scanFolders[idx_SEVD]
-
-#   use dates from scan details to sort scans into timepoints 
-#   transform date from yy-mm-dd to ddmmyyyy to match filing system of scans
-data_2[np.isnan(data_2)] = 0    # replace nan with unused date
-
-def ext(data):
-    char = datetime.strptime(str(data)[0:10], '%Y-%m-%d').strftime('%d%m%Y')
-    return char
-
-tp_baseline = []
-tp_4months = []
-tp_8months = []
-for i in range(len(data_2)):
-    tp_baseline.append(ext(data_2[i,0]))
-    tp_4months.append(ext(data_2[i,1]))
-    tp_8months.append(ext(data_2[i,2]))
-
-#   remove nan cells (in the form of 01011970) from timepoint arrays
-idx_nan_B = np.where(np.isin(tp_baseline, "01011970"))[0]
-tp_baseline = list(np.delete(tp_baseline,idx_nan_B,axis=0))
-idx_nan_4 = np.where(np.isin(tp_4months, "01011970"))[0]
-tp_4months = list(np.delete(tp_4months,idx_nan_4,axis=0))
-idx_nan_8 = np.where(np.isin(tp_8months, "01011970"))[0]
-tp_8months = list(np.delete(tp_8months,idx_nan_8,axis=0))
-
-#   use the timepoints to sort scans per timepoints
-def remove_cruft(s):
-    return s[7:-5]
-
-tp_ND = np.array([remove_cruft(s) for s in scans_ND])
-tp_MOD = np.array([remove_cruft(s) for s in scans_MOD])
-tp_MILD = np.array([remove_cruft(s) for s in scans_MILD])
-tp_SEV = np.array([remove_cruft(s) for s in scans_SEVD])
 
 
-#   extract baseline, 4-months and 8-months for the no dementia scans
-idx_ND_B = np.where(np.isin(tp_ND, np.array(tp_baseline)))[0]
-idx_ND_4 = np.where(np.isin(tp_ND, tp_4months))[0]
-idx_ND_8 = np.where(np.isin(tp_ND, tp_8months))[0]
-scans_ND_B = scans_ND[idx_ND_B]
-scans_ND_4 = scans_ND[idx_ND_4]
-scans_ND_8 = scans_ND[idx_ND_8]
-
-#   extract baseline, 4-months and 8-months for the mild dementia scans
-idx_MILD_B = np.where(np.isin(tp_MILD, tp_baseline))[0]
-idx_MILD_4 = np.where(np.isin(tp_MILD, tp_4months))[0]
-idx_MILD_8 = np.where(np.isin(tp_MILD, tp_8months))[0]
-scans_MILD_B = scans_MILD[idx_MILD_B]
-scans_MILD_4 = scans_MILD[idx_MILD_4]
-scans_MILD_8 = scans_MILD[idx_MILD_8]
-
-#   extract baseline, 4-months and 8-months for the moderate dementia scans
-idx_MOD_B = np.where(np.isin(tp_MOD, tp_baseline))[0]
-idx_MOD_4 = np.where(np.isin(tp_MOD, tp_4months))[0]
-idx_MOD_8 = np.where(np.isin(tp_MOD, tp_8months))[0]
-scans_MOD_B = scans_MOD[idx_MOD_B]
-scans_MOD_4 = scans_MOD[idx_MOD_4]
-scans_MOD_8 = scans_MOD[idx_MOD_8]
-
-#   extract baseline, 4-months and 8-months for the severe dementia scans
-idx_SEVD_B = np.where(np.isin(tp_SEV, tp_baseline))[0]
-idx_SEVD_4 = np.where(np.isin(tp_SEV, tp_4months))[0]
-idx_SEVD_8 = np.where(np.isin(tp_SEV, tp_8months))[0]
-scans_SEVD_B = scans_SEVD[idx_SEVD_B]
-scans_SEVD_4 = scans_SEVD[idx_SEVD_4]
-scans_SEVD_8 = scans_SEVD[idx_SEVD_8]
-"""
-
-
-
-
-
-
-df_1 = pd.read_excel (r'/Users/joshuaighalo/Downloads/EEG_Datasets/laurel_place/dataset/lp_clinical.xlsx')
-df_2 = pd.read_excel(r'/Users/joshuaighalo/Downloads/EEG_Datasets/laurel_place/dataset/Scan_details.xlsx')
+df_1 = pd.read_excel (r'/Users/joshuaighalo/Downloads/EEG_Datasets/laurel_place/cleaned_dataset/lp_clinical.xlsx')
+df_2 = pd.read_excel(r'/Users/joshuaighalo/Downloads/EEG_Datasets/laurel_place/cleaned_dataset/Scan_details.xlsx')
 data_1 = (df_1[['ID','MMSE']]).to_numpy()
 data_2 = (df_2[['Baseline','4 months','8 months']]).to_numpy()
 
@@ -210,7 +71,7 @@ for i in range(len(data_1)):
     data8months.append(stringMerge(data_1[:,0][i], tp_8months[i]))
 
 #   import dataset scan from directory
-search_path = r'/Users/joshuaighalo/Downloads/EEG_Datasets/laurel_place/dataset'
+search_path = r'/Users/joshuaighalo/Downloads/EEG_Datasets/laurel_place/cleaned_dataset'
 root, dirs, files = next(os.walk(search_path), ([],[],[]))
 scans = np.array(dirs)
 
