42c1e09103f3029776dfef0bb6fb2a4234bf32d9
Joshua Ighalo <70718280+iggy96@users.noreply.github.com>
2022-07-30 03:25:01 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,7 @@
+"""
+ICA using FastICA algorithm without sliding window
+"""
+
 from fn_cfg import *
 import params as cfg
 from scipy.stats import skew
@@ -67,134 +71,87 @@ def dwt(data,wavelet):
     recovered_signal = pywt.waverec(coeffs,wavelet)
     return recovered_signal
 
-
-
 hpEEG = icaHighpass(data=rawEEG,cutoff=1,fs=cfg.fs) 
 
 #   Computing ICA components
 ica = FastICA(n_components=3, random_state=0, tol=0.0001)
 comps = ica.fit_transform(hpEEG)
+comps_1 = comps[:,0]
+comps_2 = comps[:,1]
+comps_3 = comps[:,2]
 
-comps_1_win = rolling_window(comps[:,0],time,1,1)
-comps_2_win = rolling_window(comps[:,1],time,1,1)
-comps_3_win = rolling_window(comps[:,2],time,1,1)
 
 
 #   Computing kurtosis of ICA weights
-comps_1_kurtosis = kurtosis(comps_1_win,axis=1)
-comps_2_kurtosis = kurtosis(comps_2_win,axis=1)
-comps_3_kurtosis = kurtosis(comps_3_win,axis=1)
+comps_1_kurtosis = kurtosis(comps_1)
+comps_2_kurtosis = kurtosis(comps_2)
+comps_3_kurtosis = kurtosis(comps_3)
+comps_kurtosis = np.array([comps_1_kurtosis,comps_2_kurtosis,comps_3_kurtosis])
 
 #   Computing skewness of ICA weights
-comps_1_skew = skew(comps_1_win,axis=1)
-comps_2_skew = skew(comps_2_win,axis=1)
-comps_3_skew = skew(comps_3_win,axis=1)
-
-#   Computing peak-to-peak of ICA weights
-comps_1_pkpk = peaktopeak(comps_1_win)
-comps_2_pkpk = peaktopeak(comps_2_win)
-comps_3_pkpk = peaktopeak(comps_3_win)
+comps_1_skew = skew(comps_1)
+comps_2_skew = skew(comps_2)
+comps_3_skew = skew(comps_3)
+comps_skew = np.array([comps_1_skew,comps_2_skew,comps_3_skew])
 
 #   Computing sample entropy of ICA weights
-comps_1_sampEN = sample_entropy(comps_1_win)
-comps_2_sampEN = sample_entropy(comps_2_win)
-comps_3_sampEN = sample_entropy(comps_3_win)
+comps_1_sampEN = ant.sample_entropy(comps_1)
+comps_2_sampEN = ant.sample_entropy(comps_2)
+comps_3_sampEN = ant.sample_entropy(comps_3)
+comps_sampEN = np.array([comps_1_sampEN,comps_2_sampEN,comps_3_sampEN])
 
+#   Computing CI on to set threshold
+threshold_kurt = confidenceInterval(comps_kurtosis)
+threshold_skew = confidenceInterval(comps_skew)
+threshold_sampEN = confidenceInterval(comps_sampEN)
 
-#   Computing CI on kurtosis values
-threshold_kurt_1 = confidenceInterval(comps_1_kurtosis)
-threshold_kurt_2 = confidenceInterval(comps_2_kurtosis)
-threshold_kurt_3 = confidenceInterval(comps_3_kurtosis)
-threshold_skew_1 = confidenceInterval(comps_1_skew)
-threshold_skew_2 = confidenceInterval(comps_2_skew)
-threshold_skew_3 = confidenceInterval(comps_3_skew)
-threshold_pkpk_1 = confidenceInterval(comps_1_pkpk)
-threshold_pkpk_2 = confidenceInterval(comps_2_pkpk)
-threshold_pkpk_3 = confidenceInterval(comps_3_pkpk)
-threshold_sampEN_1 = confidenceInterval(comps_1_sampEN)
-threshold_sampEN_2 = confidenceInterval(comps_2_sampEN)
-threshold_sampEN_3 = confidenceInterval(comps_3_sampEN)
-
-
-
+"compare threshold with extracted parameter values"
 #   Extract epochs
-bool_ArtfCompsKurt_1 = [comps_1_kurtosis>threshold_kurt_1]
-idx_ArtfCompsKurt_1 = np.asarray(np.where(bool_ArtfCompsKurt_1[0]==True))
-bool_ArtfCompsKurt_2 = [comps_2_kurtosis>threshold_kurt_2]
-idx_ArtfCompsKurt_2 = np.asarray(np.where(bool_ArtfCompsKurt_2[0]==True))
-bool_ArtfCompsKurt_3 = [comps_3_kurtosis>threshold_kurt_3]
-idx_ArtfCompsKurt_3 = np.asarray(np.where(bool_ArtfCompsKurt_3[0]==True))
+bool_ArtfCompsKurt = [comps_kurtosis>threshold_kurt]
+idx_ArtfCompsKurt = np.asarray(np.where(bool_ArtfCompsKurt[0]==True))
+bool_ArtfCompsSkew = [comps_skew>threshold_skew]
+idx_ArtfCompsSkew = np.asarray(np.where(bool_ArtfCompsSkew[0]==True))
+bool_ArtfCompsSampEN = [comps_sampEN>threshold_sampEN]
+idx_ArtfCompsSampEN = np.asarray(np.where(bool_ArtfCompsSampEN[0]==True))
 
-bool_ArtfCompsSkew_1 = [comps_1_skew>threshold_skew_1]
-idx_ArtfCompsSkew_1 = np.asarray(np.where(bool_ArtfCompsSkew_1[0]==True))
-bool_ArtfCompsSkew_2 = [comps_2_skew>threshold_skew_2]
-idx_ArtfCompsSkew_2 = np.asarray(np.where(bool_ArtfCompsSkew_2[0]==True))
-bool_ArtfCompsSkew_3 = [comps_3_skew>threshold_skew_3]
-idx_ArtfCompsSkew_3 = np.asarray(np.where(bool_ArtfCompsSkew_3[0]==True))
-
-bool_ArtfCompsPkPk_1 = [comps_1_pkpk>threshold_pkpk_1]
-idx_ArtfCompsPkPk_1 = np.asarray(np.where(bool_ArtfCompsPkPk_1[0]==True))
-bool_ArtfCompsPkPk_2 = [comps_2_pkpk>threshold_pkpk_2]
-idx_ArtfCompsPkPk_2 = np.asarray(np.where(bool_ArtfCompsPkPk_2[0]==True))
-bool_ArtfCompsPkPk_3 = [comps_3_pkpk>threshold_pkpk_3]
-idx_ArtfCompsPkPk_3 = np.asarray(np.where(bool_ArtfCompsPkPk_3[0]==True))
-
-bool_ArtfCompsSampEN_1 = [comps_1_sampEN>threshold_sampEN_1]
-idx_ArtfCompsSampEN_1 = np.asarray(np.where(bool_ArtfCompsSampEN_1[0]==True))
-bool_ArtfCompsSampEN_2 = [comps_2_sampEN>threshold_sampEN_2]
-idx_ArtfCompsSampEN_2 = np.asarray(np.where(bool_ArtfCompsSampEN_2[0]==True))
-bool_ArtfCompsSampEN_3 = [comps_3_sampEN>threshold_sampEN_3]
-idx_ArtfCompsSampEN_3 = np.asarray(np.where(bool_ArtfCompsSampEN_3[0]==True))
-
-
-
-#   merge indices arrays
-idx_ArtfComps_1 = np.concatenate((idx_ArtfCompsKurt_1,idx_ArtfCompsSkew_1,idx_ArtfCompsPkPk_1,idx_ArtfCompsSampEN_1),axis=1).T
-idx_ArtfComps_1 = np.unique(idx_ArtfComps_1,axis=0)
-idx_ArtfComps_2 = np.concatenate((idx_ArtfCompsKurt_2,idx_ArtfCompsSkew_2,idx_ArtfCompsPkPk_2,idx_ArtfCompsSampEN_2),axis=1).T
-idx_ArtfComps_2 = np.unique(idx_ArtfComps_2,axis=0)
-idx_ArtfComps_3 = np.concatenate((idx_ArtfCompsKurt_3,idx_ArtfCompsSkew_3,idx_ArtfCompsPkPk_3,idx_ArtfCompsSampEN_3),axis=1).T
-idx_ArtfComps_3 = np.unique(idx_ArtfComps_3,axis=0)
-
+#   Merge index of components detected as artifacts by kurtosis, skewness, and sample entropy
+idx_artf_comps = np.concatenate((idx_ArtfCompsKurt,idx_ArtfCompsSkew,idx_ArtfCompsSampEN),axis=1)
+idx_artf_comps = np.unique(idx_artf_comps)
+artf_comps = comps[:,idx_artf_comps]
 
 """ 
 Apply Discrete Wavelet Transform to epochs of ICs
 remove noisy componenets within epoch
 apply inverse dwt to retrieve denoised epochs
 """
-
 wavelet = 'sym9'    
-inv_comps_1 = []
-inv_comps_2 = []
-inv_comps_3 = []
-
-artf_comps_1 = comps_1_win[idx_ArtfComps_1]
-artf_comps_1 = artf_comps_1.reshape(artf_comps_1.shape[0],artf_comps_1.shape[1]*artf_comps_1.shape[2])
-artf_comps_2 = comps_2_win[idx_ArtfComps_2]
-artf_comps_2 = artf_comps_2.reshape(artf_comps_2.shape[0],artf_comps_2.shape[1]*artf_comps_2.shape[2])
-artf_comps_3 = comps_3_win[idx_ArtfComps_3]
-artf_comps_3 = artf_comps_3.reshape(artf_comps_3.shape[0],artf_comps_3.shape[1]*artf_comps_3.shape[2])
-
-for i in range(len(idx_ArtfComps_1)):
-    inv_comps_1.append(dwt(artf_comps_1[i],wavelet))
-inv_comps_1 = np.array(inv_comps_1)
-for i in range(len(idx_ArtfComps_2)):
-    inv_comps_2.append(dwt(artf_comps_2[i],wavelet))
-inv_comps_2 = np.array(inv_comps_2)
-for i in range(len(idx_ArtfComps_3)):
-    inv_comps_3.append(dwt(artf_comps_3[i],wavelet))
-inv_comps_3 = np.array(inv_comps_3)
-
-a1 = np.arange(10)
-a1  =a1.reshape(2,5).T
-a2 = np.arange(20)
-a2 = a2[9:19]
-a2 = a2.reshape(2,5).T
-idx = np.array([0,3,4])
-
-for i in range(len(idx)):
-    a2[idx[i],:] = a1[idx[i],:]
+inv_comps = []
+for i in range(len(idx_artf_comps)):
+    inv_comps.append(dwt(artf_comps[:,i],wavelet))
+inv_comps = np.array(inv_comps)
 
 
-for i in range(len(idx_ArtfComps_1)):
-    inv_comps_1[i,:] = artf_comps_1[i,:]
+"Return dwt transformed ICs into the original windows per ICs"
+for i in range(len(idx_artf_comps)):
+    idx_inv = np.arange(len(inv_comps))
+    comps.T[idx_artf_comps[i]] = inv_comps[idx_inv[i]]
+
+
+"Recover clean signal from clean ICs"
+restored = ica.inverse_transform(comps)
+
+
+plt.plot(restored[:,0])
+plt.show()
+plt.plot(rawEEG[:,0])
+plt.show()
+
+plt.plot(restored[:,1])
+plt.show()
+plt.plot(rawEEG[:,1])
+plt.show()
+
+plt.plot(restored[:,2])
+plt.show()
+plt.plot(rawEEG[:,2])
+plt.show()
